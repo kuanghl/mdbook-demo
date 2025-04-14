98c137a6cc5d72559aecc8e892a7f1455c310657
kuanghl <kuanghl@kuanghl-pc>
2025-04-14 14:52:08 +0800

    feat(translate): selectLanguageTag add

--- books/html/assets/translate/translate_button.js
+++ books/html/assets/translate/translate_button.js
@@ -7,6 +7,9 @@
 //设置机器翻译服务通道，直接客户端本身，不依赖服务端 。相关说明参考 http://translate.zvo.cn/43086.html
 // translate.service.use("client.edge");
 
+// 语言选择下拉框
+translate.selectLanguageTag.languages = 'english,chinese_simplified,korean';
+
 // 忽略翻译的class id
 translate.ignore.class.push("icon-button");
 translate.ignore.class.push("theme-popup");
